33a765e2714670c8770a3d0c43fe33d10bdb437c
Aadya Huddar <>
2022-10-25 23:18:59 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,27 @@
+import discord
+from discord.ext import commands
+intents = discord.Intents.all()
+
+bot = commands.Bot(command_prefix='!', intents=intents)
+
+@bot.event
+async def on_connect():
+  print("Your bot is online!")
+
+@bot.command()
+async def aadya(ctx):
+  await ctx.reply("Hello from AadyaBot!")
+
+bot.run('bot token here')
+
+async def Time(ctx, time1, time2)
+  resp="yo"
+  if "am" in time2.lower():
+    resp="Good morning!"
+  elif "pm" in time2.lower():
+    if int(time1)<5:
+      resp="Good afternoon!"
+    else:
+      resp="Good evening!"
+  else:
+    resp=""
